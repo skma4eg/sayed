9ebc950bec868922a19dee9b0f3ad5968ab23208
Elsayed kamal <sayed.kamal581999@gmail.com>
2016-12-24 20:38:39 +0200

    Add files via upload

---
+++ nil.js
@@ -0,0 +1,1 @@
+﻿(function() { var html5 = ("abbr,article,aside,audio,canvas,datalist,details," + "figure,footer,header,hgroup,mark,menu,meter,nav,output," + "progress,section,time,video").split(','); for (var i = 0; i < html5.length; i++) { document.createElement(html5[i]); } try { document.execCommand('BackgroundImageCache', false, true); } catch(e) {} })(); 
